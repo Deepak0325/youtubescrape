5150d59456615feae631842d1adad76c56519030
Deepak0325 <93932817+Deepak0325@users.noreply.github.com>
2022-09-13 12:18:16 +0530

    Update video_to_s3.py

--- video_to_s3.py
+++ video_to_s3.py
@@ -1,15 +1,15 @@
-"""import pafy
+# To upload youtube video directly to amazon S3 bucket whithout downloading it to local system
+import pafy
 import requests
 import boto3
-video="https://www.youtube.com/watch?v=KNTz0N10lzg"
+video="your video link here"
 v = pafy.new(video)
 ur = v.getbest()
-watch_rul=ur.url
-print(watch_rul)
+watch_url=ur.url
 s3 = boto3.resource('s3', aws_access_key_id="Your Access key id here", aws_secret_access_key="Your Secret Access Key here")
 bucket_name = 'youvidscraper'
-r = requests.get(watch_rul, stream=True)
+r = requests.get(watch_url, stream=True)
 print(r)
 key = "youvid"
 bucket = s3.Bucket(bucket_name)
-bucket.upload_fileobj(r.raw, key)"""
+bucket.upload_fileobj(r.raw, key)
